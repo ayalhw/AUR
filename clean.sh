32d6ab7faa4c53c7e81abc0611e46b4ff95f5626
Coelacanthus <liuhongwu2003@outlook.com>
2020-09-17 15:10:22 +0800

    chore(clean.sh): add *.log

--- clean.sh
+++ clean.sh
@@ -5,6 +5,7 @@ find . -type f -iname '*.src.tar.gz' -exec rm -v {} \;
 find . -type f -iname '*.orig.tar.gz' -exec rm -v {} \;
 find . -type f -iname '*.deb' -exec rm -v {} \;
 find . -type f -iname '*.src.zip' -exec rm -v {} \;
+find . -type f -iname '*.log' -exec rm -v {} \;
 #find . -type f -iname '*.snap' -exec rm -v {} \;
 
 # Clean rules for certain packages
